016b8a8bd1ab0468d3a71a0355f4cde055479038
Ivailo <ivailosimov252@gmail.com>
2026-01-17 19:04:44 +0200

    Add Dbsets to the DbContext

--- StudyPlanner/StudyPlanner/Data/ApplicationDbContext.cs
+++ StudyPlanner/StudyPlanner/Data/ApplicationDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
+using StudyPlanner.Models;
 namespace StudyPlanner.Data
 {
     public class ApplicationDbContext : IdentityDbContext
@@ -9,5 +9,10 @@ namespace StudyPlanner.Data
             : base(options)
         {
         }
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Subject> Subjects { get; set; } = null!;
+        public virtual DbSet<StudyTask> StudyTasks { get; set; } = null!;
+        public virtual DbSet<StudySession> StudySessions { get; set; } = null!;
+
     }
 }
