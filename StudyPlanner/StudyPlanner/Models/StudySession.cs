c7ed2d9b1e0783e6aede6337737ff7f3c2f69bf7
Ivailo <ivailosimov252@gmail.com>
2026-01-17 17:56:38 +0200

    Add model StudySession

---
+++ StudyPlanner/StudyPlanner/Models/StudySession.cs
@@ -0,0 +1,24 @@
+﻿using static StudyPlanner.Common.EntityValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudyPlanner.Models
+{
+    public class StudySession
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int StudyTaskId { get; set; }
+        public StudyTask StudyTask { get; set; } = null!;
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+
+        [MaxLength(StudySessionNotesMaxLength)]
+        public string? Notes { get; set; }
+    }
+}
