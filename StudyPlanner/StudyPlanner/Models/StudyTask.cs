5896cc1f04f2e6b89117abff30a72cb2aef6c4e7
Ivailo <ivailosimov252@gmail.com>
2026-01-17 17:51:18 +0200

    Add model StudyTask

---
+++ StudyPlanner/StudyPlanner/Models/StudyTask.cs
@@ -0,0 +1,42 @@
+﻿using StudyPlanner.Common;
+using StudyPlanner.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static StudyPlanner.Common.EntityValidation;
+
+namespace StudyPlanner.Models
+{
+    public class StudyTask
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(StudyTaskTitleMinLength)]
+        [MaxLength(StudyTaskTitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+
+        [MaxLength(StudyTaskDescriptionMaxLength)]
+        public string? Description { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public TaskPriority Priority { get; set; }
+
+        [Required]
+        public Enums.TaskStatus Status { get; set; }
+
+
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+
+
+        [Required]
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; } = null!;
+    }
+}
