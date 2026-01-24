b9883bf45f7e7b50dc1def16e7c6f975a81efdb4
Ivailo <ivailosimov252@gmail.com>
2026-01-24 17:55:15 +0200

    Remove details fully from controller Category

--- StudyPlanner/StudyPlanner/Controllers/CategoryController.cs
+++ StudyPlanner/StudyPlanner/Controllers/CategoryController.cs
@@ -26,22 +26,7 @@ namespace StudyPlanner.Controllers
         }
 
         // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
+       
 
         // GET: Category/Create
         public IActionResult Create()
