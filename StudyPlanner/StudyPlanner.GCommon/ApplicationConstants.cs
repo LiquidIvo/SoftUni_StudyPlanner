bfbe836d56ca5d2402a3fedeb9363c732cef4bce
Ivailo <ivailosimov252@gmail.com>
2026-02-08 18:30:04 +0200

    Add constants for date format

--- StudyPlanner/StudyPlanner.GCommon/ApplicationConstants.cs
+++ StudyPlanner/StudyPlanner.GCommon/ApplicationConstants.cs
@@ -2,5 +2,8 @@
 {
     public static class ApplicationConstants
     {
+        public const string DateFormatForStudyTask = "yyyy-MM-dd";
+        public const string DateFormatForStudySessionForCalculations = "yyyy-MM-ddTHH:mm";
+        public const string DateFormatForStudySession = "dd MMM yyyy HH:mm";
     }
 }
